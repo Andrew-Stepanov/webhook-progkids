9310bea75b95c53484d015829bbc4bfa80848606
Andrew Stepanov <andrewstepanov@icloud.com>
2023-04-05 14:14:04 +0300

    Победил env

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
+require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
 
 // Замените этими значениями настройки своего webhook-получателя
 const webhookReceiverUrl = process.env.WEBHOOK_RECEIVER_URL;
+//const webhookReceiverUrl = 'https://cloud.roistat.com/integration/webhook?key=eb40c961ee2b6f9af08f935b26d9607a';
 
 function transformData(data) {
     const transformedData = {
